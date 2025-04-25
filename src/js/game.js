022022ebdc0dc12236ed214e287a487f703565f9
lito00356 <tomasz.liksza@gmail.com>
2025-04-25 15:27:55 +0200

    created an timer for the beavers to appear

--- src/js/game.js
+++ src/js/game.js
@@ -4,37 +4,48 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-let beaverHTML = "";
-switch (getRandomNumber(1, 9)) {
-  case 1:
-    beaverHTML = "<button class='hole hole-a' id='hole1'></button>";
-    break;
-  case 2:
-    beaverHTML = "<button class='hole hole-b' id='hole2'></button>";
-    break;
-  case 3:
-    beaverHTML = "<button class='hole hole-c' id='hole3'></button>";
-    break;
-  case 4:
-    beaverHTML = "<button class='hole hole-d' id='hole4'></button>";
-    break;
-  case 5:
-    beaverHTML = "<button class='hole hole-e' id='hole5'></button>";
-    break;
-  case 6:
-    beaverHTML = "<button class='hole hole-f' id='hole6'></button>";
-    break;
-  case 7:
-    beaverHTML = "<button class='hole hole-g' id='hole7'></button>";
-    break;
-  case 8:
-    beaverHTML = "<button class='hole hole-h' id='hole8'></button>";
-    break;
-  case 9:
-    beaverHTML = "<button class='hole hole-i' id='hole9'></button>";
-    break;
-  default:
-    beaverHTML = "";
+function beaverSelection() {
+  let beaverHTML = "";
+  switch (getRandomNumber(1, 9)) {
+    case 1:
+      beaverHTML = "<button class='hole hole-a' id='hole1'></button>";
+      break;
+    case 2:
+      beaverHTML = "<button class='hole hole-b' id='hole2'></button>";
+      break;
+    case 3:
+      beaverHTML = "<button class='hole hole-c' id='hole3'></button>";
+      break;
+    case 4:
+      beaverHTML = "<button class='hole hole-d' id='hole4'></button>";
+      break;
+    case 5:
+      beaverHTML = "<button class='hole hole-e' id='hole5'></button>";
+      break;
+    case 6:
+      beaverHTML = "<button class='hole hole-f' id='hole6'></button>";
+      break;
+    case 7:
+      beaverHTML = "<button class='hole hole-g' id='hole7'></button>";
+      break;
+    case 8:
+      beaverHTML = "<button class='hole hole-h' id='hole8'></button>";
+      break;
+    case 9:
+      beaverHTML = "<button class='hole hole-i' id='hole9'></button>";
+      break;
+    default:
+      beaverHTML = "";
+  }
+  return beaverHTML;
 }
 
-console.log(beaverHTML);
+function repeatSwitch() {
+  console.log(beaverSelection());
+}
+
+const myInterval = setInterval(repeatSwitch, 1000);
+
+setTimeout(() => {
+  clearInterval(myInterval);
+}, 15000);
