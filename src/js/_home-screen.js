d23928d351201fa10915bcf567b541bf10058289
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2025-04-26 19:28:41 +0200

    Fix home page

--- src/js/_home-screen.js
+++ src/js/_home-screen.js
@@ -1,17 +1,10 @@
-// main.js or a separate _home-page.js
-const homeScreen = document.querySelector(".home-screen");
-const gameSection = document.querySelector("#game-link");
-const tutorialSection = document.querySelector("#tutorial-link");
 const startGameButton = document.getElementById("startGameButton");
 const tutorialButton = document.getElementById("tutorialButton");
 
 startGameButton.addEventListener("click", () => {
-  homeScreen.style.display = "none";
-  gameSection.style.display = "block";
+  window.location.href = "game.html";
 });
 
 tutorialButton.addEventListener("click", () => {
-  homeScreen.style.display = "none";
-  tutorialSection.innerHTML = "<h2>How to Play</h2><p>Whack the moles!</p>";
-  tutorialSection.style.display = "block";
+  window.location.href = "tutorial.html";
 });
