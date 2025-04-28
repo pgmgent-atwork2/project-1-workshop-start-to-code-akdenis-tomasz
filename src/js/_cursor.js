ec22531680da16da4f95a73dcee74ed6ae4d5602
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2025-04-28 09:06:08 +0200

    Add audio

--- src/js/_cursor.js
+++ src/js/_cursor.js
@@ -1,5 +1,16 @@
+const sounds = [
+  new Audio("../src/assets/audio/cartoon-hammer.mp3"),
+  new Audio("../src/assets/audio/squeak-toy.mp3"),
+  new Audio("../src/assets/audio/pan.mp3")
+];
+
 document.addEventListener("mousedown", () => {
   document.documentElement.classList.add("hammer-active");
+  const randomIndex = Math.floor(Math.random() * sounds.length);
+  const sound = sounds[randomIndex];
+
+  sound.currentTime = 0;
+  sound.play();;
 });
 
 document.addEventListener("mouseup", () => {
